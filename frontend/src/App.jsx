29c6572b0aac46e335e015707656bbf81043bcd5
Sarthik <sarthikk2004@gmail.com>
2026-06-06 16:54:07 +0530

    correctionl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
-
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from "./pages/register";
 import ProtectedRoute from "./pages/ProtectedRoute";
 
 const App = () => {
